41b46708a425f9bbb55ffecb478604f46255bf5f
ololoshka2871 <SweetTreasure@2ch.hk>
2022-02-01 10:49:13 +0300

    PrettyDuration pub tuple contructor

--- src/data_unpacker.rs
+++ src/data_unpacker.rs
@@ -18,7 +18,7 @@ pub struct PageData {
     pub ft: Vec<Record>,
 }
 
-pub struct PrettyDuration(Duration);
+pub struct PrettyDuration(pub Duration);
 
 impl Display for PrettyDuration {
     fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
